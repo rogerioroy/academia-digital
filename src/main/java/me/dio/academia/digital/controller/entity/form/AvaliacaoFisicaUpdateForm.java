069558735f5022d4558894ea218f0c7c8c04a177
Rogerio Vasconcelos <rogerio_roy@hotmail.com>
2022-08-21 12:27:00 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/controller/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -0,0 +1,8 @@
+package me.dio.academia.digital.controller.entity.form;
+
+public class AvaliacaoFisicaUpdateForm {
+
+  private double peso;
+
+  private double altura;
+}
---
+++ src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,10 @@
+package me.dio.academia.digital.repository;
+
+import me.dio.academia.digital.controller.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+
+}
